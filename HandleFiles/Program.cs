fda44de2df7c32999d4c8ec32e3907180e4132d1
Firefly4266 <gaines.johnp@gmail.com>
2017-10-02 14:40:36 -0700

    File reader complete

--- HandleFiles/Program.cs
+++ HandleFiles/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace HandleFiles
 {
@@ -10,6 +11,15 @@ namespace HandleFiles
     {
         static void Main(string[] args)
         {
+            string path = @"C:\Users\Jp\Desktop\Quiz_folder\Guessing_game_ soulution.txt";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
         }
     }
 }
